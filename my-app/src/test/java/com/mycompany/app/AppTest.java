ec6feb2a5298f258f7a5d4d496300b4fddc6458a
park9n <park9n@gmail.com>
2020-03-15 17:39:18 +0900

    Practice Java (#6)

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,17 +1,36 @@
 package com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     @Test
-    public void shouldAnswerWithTrue() {
+    public void shouldAnswerWithTrue() throws IOException {
+        logger.info("Example log from {}", App.class.getSimpleName());
         App app = new App();
         assertThat(app.add(1, 1), is(2));
         assertEquals(2, app.add(1, 1));
         assertTrue(true);
+
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "test.properties";
+
+        Properties appProps = new Properties();
+        appProps.load(new FileInputStream(appConfigPath));
+        String appVersion = appProps.getProperty("version");
+
+        assertEquals("2.0", appVersion);
     }
 }
